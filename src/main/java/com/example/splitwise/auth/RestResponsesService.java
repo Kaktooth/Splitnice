8f1ae6b5f5204572b3380e4ddbc889a02ca17e0b
Volodymyr Yesypenko <volodymyryesypenko@gmail.com>
2022-01-10 03:03:00 +0200

    #3 Fix credentials getting process in RestResponsesService

--- src/main/java/com/example/splitwise/auth/RestResponsesService.java
+++ src/main/java/com/example/splitwise/auth/RestResponsesService.java
@@ -2,7 +2,6 @@ package com.example.splitwise.auth;
 
 import com.example.splitwise.controller.rest.UserAuthenticationRequest;
 import com.example.splitwise.model.User;
-import org.apache.http.HttpHost;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
@@ -11,23 +10,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -35,24 +25,11 @@ import java.util.Optional;
 @Service
 public class RestResponsesService {
 
-    private final RestTemplate restTemplate;
-
     private final ConversionService conversionService;
 
-    private final AuthenticationProvider authenticationProvider;
-
-    private final PasswordEncoder passwordEncoder;
-
     @Autowired
-    public RestResponsesService(RestTemplate restTemplate,
-                                ConversionService conversionService,
-                                AuthenticationProvider authenticationProvider,
-                                PasswordEncoder passwordEncoder) {
-
-        this.restTemplate = restTemplate;
+    public RestResponsesService(ConversionService conversionService) {
         this.conversionService = conversionService;
-        this.authenticationProvider = authenticationProvider;
-        this.passwordEncoder = passwordEncoder;
     }
 
     public UserDetails getAuthenticationUser() {
@@ -62,78 +39,55 @@ public class RestResponsesService {
     }
 
     private Optional<UserDetails> getUserFrom(Authentication authentication) {
-
         return Optional.ofNullable(((UserDetails) authentication.getPrincipal()));
 
     }
 
-    HttpHeaders httpHeaders() {
+    private HttpHeaders httpHeaders() {
 
-        String plainCreds = getAuthenticationUser().getUsername()
-            + ":" + getAuthenticationUser().getPassword();
+        String plainCredentials = getAuthenticationUser().getUsername()
+            + ":" + SecurityContextHolder.getContext().getAuthentication().getCredentials();
 
-        byte[] plainCredsBytes = plainCreds.getBytes();
-        byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
-        String base64Creds = new String(base64CredsBytes);
+        byte[] plainCredentialsBytes = plainCredentials.getBytes();
+        byte[] encodedCredentialsBytes = Base64.encodeBase64(plainCredentialsBytes);
+        String encodedCredentials = new String(encodedCredentialsBytes);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(acceptableMediaTypes());
         headers.set(HttpHeaders.CONTENT_TYPE, "application/json");
-        headers.add("Authorization", "Basic " + base64Creds);
+        headers.add("Authorization", "Basic " + encodedCredentials);
         return headers;
     }
 
     private List<MediaType> acceptableMediaTypes() {
 
-        List<MediaType> acceptableMediaTypes = new ArrayList<MediaType>();
+        List<MediaType> acceptableMediaTypes = new ArrayList<>();
         acceptableMediaTypes.add(MediaType.APPLICATION_JSON);
 
         return acceptableMediaTypes;
     }
 
-    public String getTokenForLoggedUser() {
-
-        UserDetails user = getAuthenticationUser();
-        HttpEntity<UserAuthenticationRequest>
-            userAuthenticationRequestHttpEntity =
-            new HttpEntity<>(
-                new UserAuthenticationRequest(
-                    user.getUsername(),
-                    user.getPassword()
-                ), httpHeaders());
-
-        ResponseEntity<String> response = restTemplate
-            .exchange(
-                "http://localhost:8082/sign-in",
-                HttpMethod.POST,
-                userAuthenticationRequestHttpEntity,
-                String.class);
-
-        return response.getBody();
-    }
-
     public void userRestResponse() {
         RestTemplate restTemplate = new RestTemplate();
-        HttpEntity<User> request = new HttpEntity<>(new User(), httpHeaders());
+        HttpEntity<User> request = new HttpEntity<>(null, httpHeaders());
         ResponseEntity<User> responseEntity = restTemplate.exchange(
-            "http://localhost:8082/api/account",
+            "http://localhost:8080/api/account",
             HttpMethod.GET, request, User.class
         );
     }
 
     public User findUser(Integer id) {
         RestTemplate restTemplate = new RestTemplate();
-        HttpEntity<User> request = new HttpEntity<>(new User(), httpHeaders());
+        HttpEntity<User> request = new HttpEntity<>(null, httpHeaders());
         ResponseEntity<User> responseEntity = restTemplate.exchange(
-            "http://localhost:8082/api/account/" + id,
+            "http://localhost:8080/api/account/" + id,
             HttpMethod.GET, request, User.class
         );
         return conversionService.convert(responseEntity.getBody(), User.class);
     }
 
-    HttpEntity<User> getUser(User user, HttpHeaders httpHeaders) {
-
+    public HttpEntity<User> getUser(User user, HttpHeaders httpHeaders) {
         return new HttpEntity<>(user, httpHeaders);
     }
 }
